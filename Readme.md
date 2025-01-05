b96d3762b70aab16d60378711c6021e3f1a47f05
lucaparisi91 <luca.parisi91@gmail.com>
2025-01-05 17:49:30 +0000

    Added a basic example from the gotcha documentation

--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
 # Intercept test beds
 
 A repository to test methods of intercepting external library calls linked dynamically.
+
+Compile with 
+
+```bash
+CC example.cc -lgotcha -o test
+```
---
+++ example.cc
@@ -0,0 +1,47 @@
+#include <stdio.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <fcntl.h>
+ 
+#include "gotcha/gotcha.h"
+
+typedef int (*open_fptr)(const char *pathname, int flags, mode_t mode);
+typedef FILE* (*fopen_fptr)(const char *pathname, const char *mode);
+
+static gotcha_wrappee_handle_t open_handle;
+static gotcha_wrappee_handle_t fopen_handle;
+
+static int open_wrapper(const char *pathname, int flags, mode_t mode) {
+    open_fptr open_wrappee = (open_fptr) gotcha_get_wrappee(open_handle);
+    int result = open_wrappee(pathname, flags, mode);
+    fprintf(stderr, "open(%s, %d, %u) = %d\n",
+            pathname, flags, (unsigned int) mode, result);
+    return result;
+}
+
+
+static FILE *fopen_wrapper(const char *path, const char *mode) {
+  fopen_fptr fopen_wrappee = (fopen_fptr) gotcha_get_wrappee(fopen_handle);
+  FILE *result = fopen_wrappee(path, mode);
+  fprintf(stderr, "fopen(%s, %s) = %p\n", path, mode, result);
+  return result;
+ }
+
+
+static gotcha_binding_t bindings[] = {
+  { "open", (void *)open_wrapper, &open_handle },
+  { "fopen", (void *)fopen_wrapper, &fopen_handle }
+    };
+
+
+
+int main(int argc, char *argv[]) {
+  gotcha_wrap(bindings, 2, "demotool");
+
+  open("/dev/null", O_RDONLY);
+  open("/dev/null", O_WRONLY | O_CREAT | O_EXCL);
+  fopen("/dev/random", "r");
+  fopen("/does/not/exist", "w");
+
+  return 0;
+}
