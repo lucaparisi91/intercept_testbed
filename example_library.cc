ebbfb6c984346b1ace49bc900f427f9b1115d1d1
lucaparisi91 <luca.parisi91@gmail.com>
2025-01-05 20:19:46 +0000

    Added llapi_advise wrapper

--- example_library.cc
+++ example_library.cc
@@ -4,12 +4,24 @@
  #include <fcntl.h>
  
 #include "gotcha/gotcha.h"
+#include <lustre/lustreapi.h>
+
+
+
+typedef int  (*llapi_ladvise_ptr) (int fd, unsigned long long flags,
+ int num_advise, struct llapi_lu_ladvise *ladvise);
+
+//int llapi_ladvise(int fd, unsigned long long flags,
+// int num_advise, struct llapi_lu_ladvise *ladvise);
+
 
 typedef int (*open_fptr)(const char *pathname, int flags, mode_t mode);
 typedef FILE* (*fopen_fptr)(const char *pathname, const char *mode);
 
 static gotcha_wrappee_handle_t open_handle;
 static gotcha_wrappee_handle_t fopen_handle;
+static gotcha_wrappee_handle_t llapi_advise_handle;
+
 
 static int open_wrapper(const char *pathname, int flags, mode_t mode) {
     open_fptr open_wrappee = (open_fptr) gotcha_get_wrappee(open_handle);
@@ -19,6 +31,14 @@ static int open_wrapper(const char *pathname, int flags, mode_t mode) {
     return result;
 }
 
+static int llapi_advise_wrapper(int fd, unsigned long long flags,int num_advise, struct llapi_lu_ladvise *ladvise) {
+    llapi_ladvise_ptr wrappee = (llapi_ladvise_ptr) gotcha_get_wrappee(llapi_advise_handle);
+    int result = wrappee(fd, flags, num_advise,ladvise);
+    fprintf(stderr, "llapi_advise(%d, %d, %d ,_) = %d\n",
+            fd, flags, num_advise, result);
+    
+    return result;
+}
 
 static FILE *fopen_wrapper(const char *path, const char *mode) {
   fopen_fptr fopen_wrappee = (fopen_fptr) gotcha_get_wrappee(fopen_handle);
@@ -38,7 +58,7 @@ static gotcha_binding_t bindings[] = {
 class gotcha_setup
 {
   public:
-    
+
     gotcha_setup()
     {
       gotcha_wrap(bindings, 2, "demotool");
